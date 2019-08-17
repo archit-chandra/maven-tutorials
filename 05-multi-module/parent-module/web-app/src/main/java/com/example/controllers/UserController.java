2259d5a0cdd9f8ff71799189276f7fb868cdaf16
archit_chandra <send2archit.chandra@gmail.com>
2019-08-17 22:09:48 +0200

    05-multi-module: child-module (web-app) - added controller to use those dependencies on other module

---
+++ 05-multi-module/parent-module/web-app/src/main/java/com/example/controllers/UserController.java
@@ -0,0 +1,15 @@
+package com.example.controllers;
+
+
+import com.example.domain.UserCommand;
+import com.example.entities.User;
+
+public class UserController {
+
+    User saveUser(UserCommand command) {
+
+        // fake impl
+        return new User();
+    }
+
+}
