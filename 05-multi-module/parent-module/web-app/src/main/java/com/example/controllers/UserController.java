b2c661eff5d72d0bb51b7d0054eebc96f486a51f
archit_chandra <send2archit.chandra@gmail.com>
2019-08-17 23:33:48 +0200

    05-multi-module: child-module (web-app) - used converter generated implementation in controller method

--- 05-multi-module/parent-module/web-app/src/main/java/com/example/controllers/UserController.java
+++ 05-multi-module/parent-module/web-app/src/main/java/com/example/controllers/UserController.java
@@ -1,15 +1,14 @@
 package com.example.controllers;
 
 
+import com.example.converters.UserMapper;
 import com.example.domain.UserCommand;
 import com.example.entities.User;
 
 public class UserController {
 
     User saveUser(UserCommand command) {
-
-        // fake impl
-        return new User();
+        return UserMapper.INSTANCE.userCommandToUser(command);
     }
 
 }
