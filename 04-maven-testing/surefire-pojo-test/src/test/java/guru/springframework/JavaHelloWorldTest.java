669ca261224906eca95c4d15114c1f3ef32e68d1
archit_chandra <send2archit.chandra@gmail.com>
2019-08-17 14:40:47 +0200

    04-maven-testing: surefire-junit5-test - added junit5 test class/method(s)

---
+++ 04-maven-testing/surefire-pojo-test/src/test/java/guru/springframework/JavaHelloWorldTest.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class JavaHelloWorldTest {
+
+    @Test
+    void getHello() {
+        JavaHelloWorld javaHelloWorld = new JavaHelloWorld();
+        assertEquals("Hello World", javaHelloWorld.getHello());
+    }
+}
